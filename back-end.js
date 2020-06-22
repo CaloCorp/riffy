2bde5de37edf8a474fa9eefc69e0d966fee1beb9
Calo Corp <marc.calo@codeimmersives.com>
2020-06-22 11:27:13 -0400

    seperated main.js and backend

---
+++ back-end.js
@@ -0,0 +1,14 @@
+
+
+
+
+const buyGold = function (purity,weight,price){
+
+
+let meltValue = purity/24 * weight * price
+let purchasePrice = meltValue - meltValue * .15
+
+return "I can offer " +purchasePrice + "for this piece"
+}
+
+module.exports = buyGold
--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-function getInput(i) {
+const buyGold = require('./back-end.js')
+const getInput =function(i) {
     return process.argv[i + 2];
   }
 
-
-
-const buyGold = function (purity,weight,price){
-
-
-let meltValue = purity/24 * weight * price
-let purchasePrice = meltValue - meltValue * .15
-
-return console.log("I can offer " +purchasePrice, "for this piece")
-}
-
-buyGold(getInput(0),getInput(1),getInput(2))
+console.log(buyGold(getInput(0),getInput(1),getInput(2)))
 
