993334c00dd25e0acb36e14881a53453b2e63f03
Calo Corp <marc.calo@codeimmersives.com>
2020-06-22 13:17:24 -0400

    updated front end

--- back-end.js
+++ back-end.js
@@ -8,17 +8,8 @@ const buyGold = function (purity,weight,price){
 let meltValue = purity/24 * weight * price
 let purchasePrice = meltValue - meltValue * .15
 
-if (isNaN(purity) || isNaN(weight)){
-    return "Input 3 numbers after node main.js a purity, a weight & the current spot price"}
-else if (isNaN(weight) || isNaN(price)){
-return  "Input 3 numbers after node main.js a purity, a weight & the current spot price"  
-}
-else if (purity === undefined || weight === undefined || price === undefined){
-    return "Input 3 numbers after node main.js a purity, a weight & the current spot price"}
-else if(purity < 0 || purity > 24){
-    return "Not a valid purity"
-}   
-else
+
+
     return "I can offer " +purchasePrice + "for this piece"
 }
 
--- main.js
+++ main.js
@@ -3,5 +3,20 @@ const getInput =function(i) {
     return process.argv[i + 2];
   }
 
-console.log(buyGold(getInput(0),getInput(1),getInput(2)))
+let purity= getInput(0)
+let weight= getInput(1)
+let price = getInput(2)
 
+if (isNaN(purity) || isNaN(weight)){
+    console.log("Input 3 numbers after node main.js a purity, a weight & the current spot price")}
+else if (isNaN(weight) || isNaN(price)){
+    console.log("Input 3 numbers after node main.js a purity, a weight & the current spot price")  
+}
+else if (purity === undefined || weight === undefined || price === undefined){
+    console.log("Input 3 numbers after node main.js a purity, a weight & the current spot price")}
+else if(purity < 0 || purity > 24){
+     console.log("Not a valid purity")}
+
+     else console.log(buyGold(purity,weight,price))
+
+  
