2c6f2b9492923c02e62904f280ece7c39c0179f5
Calo Corp <marc.calo@codeimmersives.com>
2020-06-22 11:47:59 -0400

    added extra functionality to code

--- back-end.js
+++ back-end.js
@@ -8,6 +8,10 @@ const buyGold = function (purity,weight,price){
 let meltValue = purity/24 * weight * price
 let purchasePrice = meltValue - meltValue * .15
 
+if (isNaN(purity) || isNaN(weight) || isNaN(price) === false )
+return "Input 3 numbers after node main.js a purity, a weight & the current spot price "
+
+else
 return "I can offer " +purchasePrice + "for this piece"
 }
 
